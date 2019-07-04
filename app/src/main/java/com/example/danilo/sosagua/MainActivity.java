1b482c20603c26c375e64ba1628d75a8d644921b
danilo49 <danilopx1@gmail.com>
2019-07-04 00:30:10 -0300

    .

--- app/src/main/java/com/example/danilo/sosagua/MainActivity.java
+++ app/src/main/java/com/example/danilo/sosagua/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature (Window.FEATURE_NO_TITLE); // irá esconder o título
+        getSupportActionBar (). hide ();
         setContentView(R.layout.activity_main);
         //Toast.makeText(this, "Bem vindo!", Toast.LENGTH_SHORT).show();
         if(isServicesOK()){
